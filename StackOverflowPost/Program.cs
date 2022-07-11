3446d8eed223102e95943d2a49f8a1ad7c211929
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-11 07:41:03 +0200

    Add project files.

---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// https://drive.google.com/open?id=106X9o_Y0jR80vw50RZpiph24LKRRY0JV&authuser=arzej2k%40gmail.com&usp=drive_fs
+// Exercise 2: Design a StackOverflow Post
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationTime { get; set; }
+        public Post(string postTitle, string postDesc, DateTime postTime)
+        {
+            this.Title = postTitle;
+            this.Description = postDesc;
+            this.CreationTime = postTime;
+        }
+        public void UpVote()
+        {
+            Console.WriteLine("Upvote!");
+            List<string> upVoteList = new List<string>();
+            while (true)
+            {
+                var userUpVoteInput = Console.ReadLine();
+                upVoteList.Add(userUpVoteInput);
+                if (userUpVoteInput == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine("\nUpvote Count: " + upVoteList.Count);
+        }
+        public void DownVote()
+        {
+            Console.WriteLine("Downvote!");
+            List<string> downVoteList = new List<string>();
+            while (true)
+            {
+                var userDownVoteInput = Console.ReadLine();
+                downVoteList.Add(userDownVoteInput);
+                if (userDownVoteInput == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine("\nDownvote Count: " + downVoteList.Count);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Post Title: ");
+            var title = Console.ReadLine();
+            Console.WriteLine("Post Description: ");
+            var description = Console.ReadLine();
+            var creationTime = DateTime.Now;
+
+            Post post = new Post(title, description, creationTime);
+            Console.WriteLine("Type 'exit' to exit.");
+            post.UpVote();
+            post.DownVote();
+        }
+    }
+}
